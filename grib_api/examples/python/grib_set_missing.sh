419a6c298ef69d6355ebc99cef3daadf66feee99
jnyrup <jnyrup@gmail.com>
2020-05-18 13:41:00 +0200

    remove eccodes 2.4.1

--- grib_api/examples/python/grib_set_missing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. ./include.sh
-
-REDIRECT=/dev/null
-$PYTHON $examples_src/grib_set_missing.py 2> $REDIRECT > $REDIRECT
-rm -f out.p_set_missing.grib
--- grib_api/examples/python/grib_set_pv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. ./include.sh
-
-OUTPUT=grib_set_pv.py.temp.grib
-$PYTHON $examples_src/grib_set_pv.py
-rm $OUTPUT
